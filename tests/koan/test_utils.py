820367ba69f32eaa1682708b7c06061781fb4b7d
SchoolGuy <egotthold@suse.de>
2019-08-26 08:57:15 +0200

    Remove not needed test

--- tests/koan/test_utils.py
+++ tests/koan/test_utils.py
@@ -4,17 +4,6 @@ import distro
 from koan import utils
 
 
-def test_check_dist():
-    # Arrange
-    distro.id = MagicMock(return_value="suse")
-
-    # Act
-    res = utils.check_dist()
-
-    # Assert
-    assert res in ["debian", "suse", "redhat"]
-
-
 def test_os_release():
     # Arrange
     distro.linux_distribution = MagicMock(return_value=("suse", 11, "codename"))
