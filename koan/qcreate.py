6e5ad306fd54db4656cce74756af6fc1c9626261
Enno Gotthold <matrixfueller@gmail.com>
2021-07-24 10:31:54 +0200

    Fix incorrect raise of an InfoException in qcreate.py

--- koan/qcreate.py
+++ koan/qcreate.py
@@ -51,4 +51,4 @@ def start_install(*args, **kwargs):
     cmd = virtinstall.build_commandline("qemu:///system", *args, **kwargs)
     rc, result, result_stderr = utils.subprocess_get_response(cmd, ignore_rc=True, get_stderr=True)
     if rc != 0:
-        raise utils.InfoException("command failed (%s): %s %s" % (rc, result, result_stderr))
+        raise InfoException("command failed (%s): %s %s" % (rc, result, result_stderr))
