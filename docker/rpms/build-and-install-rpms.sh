d8d3e7488e49c7c4695209da4520abf710ce8306
Enno Gotthold <enno.gotthold@suse.com>
2025-08-01 10:25:59 +0200

    Docker: Add exception for openSUSE Leap

--- docker/rpms/build-and-install-rpms.sh
+++ docker/rpms/build-and-install-rpms.sh
@@ -22,7 +22,11 @@ docker build -t "$IMAGE" -f "$DOCKERFILE" .
 # Build RPMs
 echo "==> Build RPMs ..."
 mkdir -p rpm-build
-docker run -ti -v "$PWD/rpm-build:/usr/src/koan/rpm-build" "$IMAGE"
+if [ "$TAG" == "opensuse-leap" ]; then
+    docker run -ti -v "$PWD/rpm-build:/usr/src/koan/rpm-build" "$IMAGE" make PYTHON=/usr/bin/python3.11 rpms
+else
+    docker run -ti -v "$PWD/rpm-build:/usr/src/koan/rpm-build" "$IMAGE"
+fi
 
 # Launch container and install koan
 echo "==> Start container ..."
